efc8ad84df47861ff4f508a24f3b7e5e268f5729
James Rhodes <james.rhodes@hotmail.com.au>
2024-02-27 00:20:05 +1100

    got running for all files against all files

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 
 use ignore::{DirEntry, Walk};
+use ncd::NormalizedCompressedDistance;
 pub mod ncd;
 
 fn is_hidden(entry: &DirEntry) -> bool {
@@ -25,19 +26,40 @@ fn main() -> Result<(), Box<dyn Error>> {
     let walker = Walk::new("./test_data");
     let paths: Vec<std::path::PathBuf> = walker.filter_map(get_file_paths).collect();
 
-    let curr = std::time::Instant::now();
-    let res = ncd::get_n_most_similar_files(5, &paths[1], &paths)?;
-    println!("Time: {:?}", curr.elapsed());
+    let results: Vec<(String, Vec<NormalizedCompressedDistance>)> = paths
+        .iter()
+        .map(|p| {
+            (
+                p.to_str().unwrap().to_owned(),
+                ncd::get_n_most_similar_files(5, p, &paths).unwrap(),
+            )
+        })
+        .collect();
+    for res in results {
+        for ncd in res.1 {
+            println!(
+                "{} -> {} SCORE: {}",
+                res.0,
+                ncd.path.to_str().unwrap(),
+                ncd.ncd_value
+            )
+        }
 
-    println!("NCDS: {:?}", res);
-    for ncd in res {
-        println!(
-            "{} -> {} SCORE: {}",
-            paths[1].to_str().unwrap(),
-            ncd.path.to_str().unwrap(),
-            ncd.ncd_value
-        );
+        println!("----------------------------------")
     }
+    // let curr = std::time::Instant::now();
+    // let res = ncd::get_n_most_similar_files(5, &paths[1], &paths)?;
+    // println!("Time: {:?}", curr.elapsed());
+    //
+    // println!("NCDS: {:?}", res);
+    // for ncd in res {
+    //     println!(
+    //         "{} -> {} SCORE: {}",
+    //         paths[1].to_str().unwrap(),
+    //         ncd.path.to_str().unwrap(),
+    //         ncd.ncd_value
+    //     );
+    // }
 
     Ok(())
 }
